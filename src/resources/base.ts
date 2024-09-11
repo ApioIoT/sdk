b75fed9038b6e432428184fb8196985ba21ae584
Gianluigi Provinciali <gianluigi.provinciali@gmail.com>
2024-09-11 15:46:17 +0200

    chore: code refactor

--- src/resources/base.ts
+++ src/resources/base.ts
@@ -1,6 +1,58 @@
-import { BaseResponse } from '../types'
+import { AxiosInstance, AxiosResponse } from 'axios'
 import Resource from './resource'
+import { ApioResponse, BaseResponse } from '../types'
+import { handleException } from '../utils'
 
-class BaseResource<T, K extends BaseResponse> extends Resource<T, K> {}
+class BaseResource<T, K extends BaseResponse> extends Resource<T, K> {
+  private path: string
+
+  constructor(client: AxiosInstance, path: string){
+    super(client)
+    this.path = path
+  }
+
+  async findAll(): Promise<Array<K> | never> {
+    try {
+      const { data } = await this.client.get<ApioResponse<Array<K>>>(this.path)
+      return data.data!
+    } catch (e) {
+      handleException(e)
+    }
+  }
+
+  async findById(uuid: string): Promise<K | never> {
+    try {
+      const { data } = await this.client.get<ApioResponse<K>>(`${this.path}/${uuid}`)
+      return data.data!
+    } catch (e) {
+      handleException(e)
+    }
+  }
+
+  async create(data: T): Promise<string | never> {
+    try {
+      const { data: dataRes } = await this.client.post<object, AxiosResponse<ApioResponse<K>>, T>(this.path, data)
+      return dataRes.data!.uuid
+    } catch (e) {
+      handleException(e)
+    }
+  }
+
+  async updateById(uuid: string, data: T): Promise<undefined | never> {
+    try {
+      await this.client.put(`${this.path}/${uuid}`, data)
+    } catch (e) {
+      handleException(e)
+    }
+  }
+
+  async deleteById(uuid: string): Promise<undefined | never> {
+    try {
+      await this.client.delete(`${this.path}/${uuid}`)
+    } catch (e) {
+      handleException(e)
+    }
+  }
+}
 
 export default BaseResource
--- src/resources/resource.ts
+++ src/resources/resource.ts
@@ -1,58 +1,22 @@
-import { AxiosInstance, AxiosResponse } from 'axios'
-import { handleException } from '../utils'
-import { ApioResponse, BaseResponse } from '../types'
+import { AxiosInstance } from 'axios'
+import { BaseResponse } from '../types'
 
+/**
+ * Platform resource.
+ * 
+ * @param client Axios Instance
+ * 
+ * @template T Request type (es. NewDevice)
+ * @template K Response type contains uuid property (es. Device)
+ */
 abstract class Resource<T, K extends BaseResponse> {
-  private client: AxiosInstance
-  private path: string
+  constructor (protected client: AxiosInstance) {}
 
-  constructor (client: AxiosInstance, path: string) {
-    this.client = client
-    this.path = path
-  }
-
-  async findAll(): Promise<Array<K> | never> {
-    try {
-      const { data } = await this.client.get<ApioResponse<Array<K>>>(this.path)
-      return data.data!
-    } catch (e) {
-      handleException(e)
-    }
-  }
-
-  async findById(uuid: string): Promise<K | never> {
-    try {
-      const { data } = await this.client.get<ApioResponse<K>>(`${this.path}/${uuid}`)
-      return data.data!
-    } catch (e) {
-      handleException(e)
-    }
-  }
-
-  async create(data: T): Promise<string | never> {
-    try {
-      const { data: dataRes } = await this.client.post<object, AxiosResponse<ApioResponse<K>>, T>(this.path, data)
-      return dataRes.data!.uuid
-    } catch (e) {
-      handleException(e)
-    }
-  }
-
-  async updateById(uuid: string, data: T): Promise<undefined | never> {
-    try {
-      await this.client.put(`${this.path}/${uuid}`, data)
-    } catch (e) {
-      handleException(e)
-    }
-  }
-
-  async deleteById(uuid: string): Promise<undefined | never> {
-    try {
-      await this.client.delete(`${this.path}/${uuid}`)
-    } catch (e) {
-      handleException(e)
-    }
-  }
+  abstract findAll(): Promise<Array<K> | never>
+  abstract findById(uuid: string): Promise<K | never>
+  abstract create(data: T): Promise<string | never>
+  abstract updateById(uuid: string, data: T): Promise<undefined | never>
+  abstract deleteById(uuid: string): Promise<undefined | never>
 }
 
 export default Resource
