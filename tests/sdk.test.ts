b33ceb80178a2c88d99b4b252e5fdab89377470b
Gianluigi Provinciali <gianluigi.provinciali@gmail.com>
2025-10-30 10:09:27 +0100

    refactor: use nock and node:test instead real api and jest

--- tests/sdk.test.ts
+++ tests/sdk.test.ts
@@ -1,174 +1,407 @@
-import 'dotenv/config'
+import { before, test } from 'node:test'
+import { strictEqual } from 'node:assert'
+import nock from 'nock'
 
 import Sdk from '../src/sdk/sdk'
-
 import { AuthenticationError, ConfigurationError, NotFoundError, AbortError } from '../src/types'
 
-const sdk = new Sdk({
-  uri: process.env.BASE_URI!,
-  authorization: {
-    type: 'apiKey',
-    secret: process.env.API_KEY!
+import projects from './mock/projects.json'
+import project from './mock/project.json'
+import nodes from './mock/nodes.json'
+import node from './mock/node.json'
+import devices from './mock/devices.json'
+import device from './mock/device.json'
+import deviceType from './mock/devicetype.json'
+import deviceTypes from './mock/devicetypes.json'
+import rules from './mock/rules.json'
+import rule from './mock/rule.json'
+
+const API_URI = 'http://api.example.com'
+const API_KEY = 'my-api-key'
+const PROJECT_ID = 'my-project-id'
+const NODE_ID = 'my-node-id'
+const DEVICE_ID = 'my-device-id'
+const DEVICE_TYPE_ID = 'my-devicetype-id'
+const RULE_ID = 'my-rule-id'
+
+before(() => {
+  nock(API_URI)
+    .persist()
+    .matchHeader('Authorization', `apiKey ${API_KEY}`)
+    .get('/projects')
+    .reply(200, projects)
+  nock(API_URI)
+    .persist()
+    .matchHeader('Authorization', `apiKey ${API_KEY}`)
+    .get(`/projects/${PROJECT_ID}/`)
+    .reply(200, project)
+  nock(API_URI)
+    .persist()
+    .get(`/projects/${PROJECT_ID}/nodes`)
+    .reply(function () {
+      const auth = this.req.headers['authorization']
+      if (auth === `apiKey ${API_KEY}`) {
+        return [200, nodes]
+      } else {
+        return [401, {}]
+      }
+    })
+  nock(API_URI)
+    .persist()
+    .matchHeader('Authorization', `apiKey ${API_KEY}`)
+    .get(`/projects/${PROJECT_ID}/nodes/${NODE_ID}`)
+    .reply(200, node)
+  nock(API_URI)
+    .persist()
+    .matchHeader('Authorization', `apiKey ${API_KEY}`)
+    .get(`/projects/${PROJECT_ID}/devices`)
+    .reply(200, devices)
+  nock(API_URI)
+    .persist()
+    .matchHeader('Authorization', `apiKey ${API_KEY}`)
+    .get(`/projects/${PROJECT_ID}/deviceTypes`)
+    .reply(200, deviceTypes)
+  nock(API_URI)
+    .persist()
+    .matchHeader('Authorization', `apiKey ${API_KEY}`)
+    .get(`/projects/${PROJECT_ID}/deviceTypes/${DEVICE_TYPE_ID}`)
+    .reply(200, deviceType)
+  nock(API_URI)
+    .persist()
+    .matchHeader('Authorization', `apiKey ${API_KEY}`)
+    .get(`/projects/${PROJECT_ID}/devices/${DEVICE_ID}`)
+    .reply(200, device)
+  nock(API_URI)
+    .persist()
+    .matchHeader('Authorization', `apiKey ${API_KEY}`)
+    .get(`/projects/${PROJECT_ID}/rules`)
+    .reply(200, rules)
+  nock(API_URI)
+    .persist()
+    .matchHeader('Authorization', `apiKey ${API_KEY}`)
+    .get(`/projects/${PROJECT_ID}/rules`)
+    .query({ enabled: true, mode: 'edge' })
+    .reply(200, rules)
+  nock(API_URI)
+    .persist()
+    .matchHeader('Authorization', `apiKey ${API_KEY}`)
+    .get(`/projects/${PROJECT_ID}/rules/${RULE_ID}`)
+    .reply(200, rule)
+  nock(API_URI)
+    .persist()
+    .matchHeader('Authorization', `apiKey ${API_KEY}`)
+    .put(`/projects/${PROJECT_ID}/rules/${RULE_ID}`)
+    .reply(200, {})
+})
+
+test('test ConfigurationError', async () => {
+  const sdk = new Sdk({
+    uri: 'asd',
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
+  })
+  const project = sdk.project(PROJECT_ID)
+
+  try {
+    await project.node.findAll()
+  } catch (e) {
+    if (!(e instanceof ConfigurationError)) {
+      throw new Error('Error must be ConfigurationError')
+    }
   }
 })
-const project = sdk.project(process.env.PROJECT_ID!)
 
-describe('Testing SDK', () => {
-  test('test ConfigurationError', async () => {
-    const sdk = new Sdk({
-      uri: 'asd',
-      authorization: {
-        type: 'apiKey',
-        secret: process.env.API_KEY!
-      }
-    }).project(process.env.PROJECT_ID!)
-
-    await expect(sdk.node.findAll())
-      .rejects
-      .toThrow(ConfigurationError)
+test('test AuthenticationError', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: 'lol'
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('test AuthenticationError', async () => {
-    const sdk = new Sdk({
-      uri: process.env.BASE_URI!,
-      authorization: {
-        type: 'apiKey',
-        secret: 'lol'
-      }
-    }).project(process.env.PROJECT_ID!)
+  try {
+    await project.node.findAll()
+  } catch (e) {
+    if (!(e instanceof AuthenticationError)) {
+      throw new Error('Error must be AuthenticationError')
+    }
+  }
+})
 
-    await expect(sdk.node.findAll())
-      .rejects
-      .toThrow(AuthenticationError)
+test('get nodes', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('get nodes', async () => {
-    await expect(project.node.findAll())
-      .resolves
-      .toBeInstanceOf(Array)
+  const nodes = await project.node.findAll()
+  if (!(nodes instanceof Array)) {
+    throw new Error('Nodes Types must be array')
+  }
+})
+
+test('get node', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('get node', async () => {
-    await expect(project.node.findById(process.env.TEST_NODE_UUID!))
-      .resolves
-      .toHaveProperty('uuid', process.env.TEST_NODE_UUID!)
+  const node = await project.node.findById(NODE_ID)
+  strictEqual(node.uuid, NODE_ID)
+})
+
+test('get fake node', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('get fake node', async () => {
-    await expect(project.node.findById('123'))
-      .rejects
-      .toThrow(NotFoundError)
+  try {
+    await project.node.findById('123')
+  } catch (e) {
+    if (!(e instanceof NotFoundError)) {
+      throw new Error('Error must be NotFoundError')
+    }
+  }
+})
+
+test('get devices', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('get devices', async () => {
-    await expect(project.device.findAll())
-      .resolves
-      .toBeInstanceOf(Array)
+  const devices = await project.device.findAll()
+  if (!(devices instanceof Array)) {
+    throw new Error('Devices must be array')
+  }
+})
+
+test('get device', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('get device', async () => {
-    await expect(project.device.findById(process.env.TEST_DEVICE_UUID!))
-      .resolves
-      .toHaveProperty('uuid', process.env.TEST_DEVICE_UUID!)
+  const device = await project.device.findById(DEVICE_ID)
+  strictEqual(device.uuid, DEVICE_ID)
+})
+
+test('get fake device', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('get fake device', async () => {
-    await expect(project.device.findById('123'))
-      .rejects
-      .toThrow(NotFoundError)
+  try {
+    await project.device.findById('123')
+  } catch (e) {
+    if (!(e instanceof NotFoundError)) {
+      throw new Error('Error must be NotFoundError')
+    }
+  }
+})
+
+test('get device types', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('get device types', async () => {
-    await expect(project.deviceType.findAll())
-      .resolves
-      .toBeInstanceOf(Array)
+  const deviceTypes = await project.deviceType.findAll()
+  if (!(deviceTypes instanceof Array)) {
+    throw new Error('Device Types must be array')
+  }
+})
+
+test('get device type', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('get device type', async () => {
-    await expect(project.deviceType.findById(process.env.TEST_DEVICE_TYPE_UUID!))
-      .resolves
-      .toHaveProperty('uuid', process.env.TEST_DEVICE_TYPE_UUID!)
+  const deviceType = await project.deviceType.findById(DEVICE_TYPE_ID)
+  strictEqual(deviceType.uuid, DEVICE_TYPE_ID)
+})
+
+test('get fake device type', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('get fake device type', async () => {
-    await expect(project.deviceType.findById('123'))
-      .rejects
-      .toThrow(NotFoundError)
+  try {
+    await project.deviceType.findById('123')
+  } catch (e) {
+    if (!(e instanceof NotFoundError)) {
+      throw new Error('Error must be NotFoundError')
+    }
+  }
+})
+
+test('test Timeout', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: 'lol'
+    },
+    timeout: 1
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('test Timeout', async () => {
-    const sdk = new Sdk({
-      uri: process.env.BASE_URI!,
-      authorization: {
-        type: 'apiKey',
-        secret: 'lol'
-      },
-      timeout: 1
-    }).project(process.env.PROJECT_ID!)
+  try {
+    await project.node.findAll()
+  } catch (e) {
+    if (!(e instanceof AbortError)) {
+      throw new Error('Error must be AbortError')
+    }
+  }
+})
 
-    await expect(sdk.node.findAll())
-      .rejects
-      .toThrow(AbortError)
+test('test Cancellation with timeout', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: 'lol'
+    },
+    signal: AbortSignal.timeout(1)
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('test Cancellation with timeout', async () => {
-    const sdk = new Sdk({
-      uri: process.env.BASE_URI!,
-      authorization: {
-        type: 'apiKey',
-        secret: 'lol'
-      },
-      signal: AbortSignal.timeout(1)
-    }).project(process.env.PROJECT_ID!)
+  try {
+    await project.node.findAll()
+  } catch (e) {
+    if (!(e instanceof AbortError)) {
+      throw new Error('Error must be AbortError')
+    }
+  }
+})
 
-    await expect(sdk.node.findAll())
-      .rejects
-      .toThrow(AbortError)
+test('test Cancellation with abort', async () => {
+  const controller = new AbortController()
+  
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: 'lol'
+    },
+    signal: controller.signal
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('test Cancellation with abort', async () => {
-    const controller = new AbortController()
-   
-    const sdk = new Sdk({
-      uri: process.env.BASE_URI!,
-      authorization: {
-        type: 'apiKey',
-        secret: 'lol'
-      },
-      signal: controller.signal
-    }).project(process.env.PROJECT_ID!)
+  controller.abort()
 
-    controller.abort()
+  try {
+    await project.node.findAll()
+  } catch (e) {
+    if (!(e instanceof AbortError)) {
+      throw new Error('Error must be AbortError')
+    }
+  }
+})
 
-    await expect(sdk.node.findAll())
-      .rejects
-      .toThrow(AbortError)
+test('get rules', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('get rules', async () => {
-    await expect(project.rule.findAll())
-      .resolves
-      .toBeInstanceOf(Array)
-  })
+  const rules = await project.rule.findAll()
+  if (!(rules instanceof Array)) {
+    throw new Error('Rules must be array')
+  }
+})
 
-  test('get rule', async () => {
-    await expect(project.rule.findById(process.env.TEST_RULE_UUID!))
-      .resolves
-      .toHaveProperty('uuid', process.env.TEST_RULE_UUID!)
+test('get rule', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
 
-  test('get edge rules', async () => {
-    await expect(project.rule.findAll({
-      mode: 'edge',
-      enabled: true
-    }))
-      .resolves
-      .toBeInstanceOf(Array)
-  })
+  const rule = await project.rule.findById(RULE_ID)
+  strictEqual(rule.uuid, RULE_ID)
+})
 
-  test('test update rule', async () => {
-    const rule = await project.rule.findAll()
-    await expect(project.rule.updateById(rule[0].uuid, rule[0]))
-      .resolves
+test('get edge rules', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
   })
+  const project = sdk.project(PROJECT_ID)
+
+  const rules = await project.rule.findAll({
+    mode: 'edge',
+    enabled: true
+  })
+  if (!(rules instanceof Array)) {
+    throw new Error('Rules must be array')
+  }
+})
+
+test('test update rule', async () => {
+  const sdk = new Sdk({
+    uri: API_URI,
+    authorization: {
+      type: 'apiKey',
+      secret: API_KEY
+    }
+  })
+  const project = sdk.project(PROJECT_ID)
+
+  const rule = await project.rule.findAll()
+  await project.rule.updateById(rule[0].uuid, rule[0])
 })
